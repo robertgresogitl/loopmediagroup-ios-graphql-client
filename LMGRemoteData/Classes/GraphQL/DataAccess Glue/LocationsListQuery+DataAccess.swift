00203f78db6911530107815e0bec4b2032013127
Cayley Humphries <cayley.humphries@gmail.com>
2019-02-11 12:07:25 -0800

    Add missing data access glue for OfferLocationItem

--- LMGRemoteData/Classes/GraphQL/DataAccess Glue/LocationsListQuery+DataAccess.swift
+++ LMGRemoteData/Classes/GraphQL/DataAccess Glue/LocationsListQuery+DataAccess.swift
@@ -30,3 +30,28 @@ extension LocationItem.Address {
         return LMGDAAddress(id: id, streetAddress: street, postalCode: postalcode, city: city, province: state, country: country, coordinate: coordinateBuider.build(), suite: suite, building: streetAddress)
     }
 }
+
+
+// OfferLocationItem is a special query for LocationItem that does NOT
+// include a shareUrl (not available on the node in the Graph API).
+extension OfferLocationItem {
+    
+    func toDataAccess(business: LMGDABusiness?) -> LMGDALocation {
+        let builder = LMGDALocationBuilder(id, address: address.toDataAccess(), name: name)
+        builder.distance = distance != nil ? NSNumber(value: distance!) : nil
+        builder.phoneNumber = phone
+        builder.urlPath = url
+        builder.business = business
+        return builder.build()
+    }
+}
+
+extension OfferLocationItem.Address {
+    
+    func toDataAccess() -> LMGDAAddress {
+        let coordinateBuider = LMGDACoordinateBuilder()
+        coordinateBuider.longitude = centre.first!
+        coordinateBuider.latitude = centre.last!
+        return LMGDAAddress(id: id, streetAddress: street, postalCode: postalcode, city: city, province: state, country: country, coordinate: coordinateBuider.build(), suite: suite, building: streetAddress)
+    }
+}
